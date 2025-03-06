099bc80d8e5f8b31063a36b17af7f80ef4bdbc6f
bete7512 <betekbebe@gmail.com>
2025-03-06 17:55:20 -0500

    feat: add makefile, update .gitignore, and implement MongoDB error handling in repository factory

---
+++ database/dbClient.go
@@ -0,0 +1,157 @@
+// auth/database/client.go
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bete7512/goauth/models"
+	"github.com/bete7512/goauth/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBClient interface {
+	Connect() error
+	Close() error
+	GetDB() interface{}
+}
+
+type PostgresClient struct {
+	Config      *types.DatabaseConfig
+	DB          *gorm.DB
+	URL         string
+	AutoMigrate bool
+}
+
+type MySQLClient struct {
+	Config      *types.DatabaseConfig
+	DB          *gorm.DB
+	URL         string
+	AutoMigrate bool
+}
+
+type MongoDBClient struct {
+	Config      *types.DatabaseConfig
+	DB          *mongo.Client
+	URL         string
+	AutoMigrate bool
+}
+
+func NewDBClient(config types.DatabaseConfig) (DBClient, error) {
+	switch config.Type {
+	case types.PostgreSQL:
+		return &PostgresClient{Config: &config, URL: config.URL, AutoMigrate: config.AutoMigrate}, nil
+	case types.MySQL:
+		return &MySQLClient{Config: &config, URL: config.URL, AutoMigrate: config.AutoMigrate}, nil
+	case types.MongoDB:
+		return &MongoDBClient{Config: &config, URL: config.URL, AutoMigrate: config.AutoMigrate}, nil
+	default:
+		return nil, fmt.Errorf("unsupported database type: %s", config.Type)
+	}
+}
+
+func (c *PostgresClient) Connect() error {
+	db, err := gorm.Open(postgres.Open(c.URL), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to PostgreSQL: %w", err)
+	}
+	if c.AutoMigrate {
+		if err := db.AutoMigrate(
+			&models.User{},
+			&models.Session{},
+		); err != nil {
+			return fmt.Errorf("failed to auto-migrate: %w", err)
+		}
+	}
+
+	c.DB = db
+	return nil
+}
+
+func (c *PostgresClient) Close() error {
+	if c.DB != nil {
+		sqlDB, err := c.DB.DB()
+		if err != nil {
+			return err
+		}
+		return sqlDB.Close()
+	}
+	return nil
+}
+
+func (c *PostgresClient) GetDB() interface{} {
+	return c.DB
+}
+
+func (c *MySQLClient) Connect() error {
+	db, err := gorm.Open(mysql.Open(c.URL), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to MySQL: %w", err)
+	}
+	if c.AutoMigrate {
+		if err := db.AutoMigrate(
+			&models.User{},
+			&models.Session{},
+		); err != nil {
+			return fmt.Errorf("failed to auto-migrate: %w", err)
+		}
+	}
+
+	c.DB = db
+	return nil
+}
+
+func (c *MySQLClient) Close() error {
+	if c.DB != nil {
+		sqlDB, err := c.DB.DB()
+		if err != nil {
+			return err
+		}
+		return sqlDB.Close()
+	}
+	return nil
+}
+
+func (c *MySQLClient) GetDB() interface{} {
+	return c.DB
+}
+
+func (c *MongoDBClient) Connect() error {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(c.URL)
+	
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+	
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+	
+	c.DB = client
+	if c.AutoMigrate {
+		// TODO: do some research on MongoDB migrations
+		// at least check if the collections exist and create them if they don't exist
+	}
+	
+	return nil
+}
+
+func (c *MongoDBClient) Close() error {
+	if c.DB != nil {
+		return c.DB.Disconnect(context.TODO())
+	}
+	return nil
+}
+
+func (c *MongoDBClient) GetDB() interface{} {
+	return c.DB
+}
--- repositories/factory.go
+++ repositories/factory.go
@@ -18,7 +18,16 @@ func NewRepositoryFactory(dbType types.DatabaseType, db interface{}) (interfaces
 			return nil, fmt.Errorf("invalid database connection for PostgreSQL")
 		}
 		return postgres.NewRepositoryFactory(gormDB), nil
-	// TODO: continue implementing other database types
+	case types.MongoDB:
+		// mongoClient, ok := db.(*mongo.Client)
+		// if !ok {
+		// 	return nil, fmt.Errorf("invalid database connection for MongoDB")
+		// }
+		// // You need to create a MongoDB repository factory implementation
+		// return mongodb.NewRepositoryFactory(mongoClient), nil
+		// TODO: continue implementing other database types
+		return nil, fmt.Errorf("MongoDB not implemented")
+
 	default:
 		return nil, fmt.Errorf("unsupported database type: %s", dbType)
 	}
